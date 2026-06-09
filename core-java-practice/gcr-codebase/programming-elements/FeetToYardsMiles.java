c19c8448469b94ba7d6eb425a8c671cfc88b15e3
naman srivastav <namansrivastav@namans-MacBook-Air-2.local>
2026-06-09 10:11:37 +0530

    Add Programming Elements Level2 Java practice files

---
+++ core-java-practice/gcr-codebase/programming-elements/FeetToYardsMiles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FeetToYardsMiles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = scanner.nextDouble();
+
+        double distanceInYards = distanceInFeet / 3.0;
+        double distanceInMiles = distanceInYards / 1760.0;
+
+        System.out.printf("The distance in yards is %.2f while the distance in miles is %.5f%n",
+                distanceInYards, distanceInMiles);
+
+        scanner.close();
+    }
+}
